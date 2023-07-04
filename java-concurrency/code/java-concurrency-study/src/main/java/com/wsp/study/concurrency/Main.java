8125836f7b853443688fa2011da118f2ae1618f6
ship <594117065@qq.com>
2023-07-04 08:03:30 +0800

    package-info 添加

--- java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/Main.java
+++ java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/Main.java
@@ -1,4 +1,14 @@
 package com.wsp.study.concurrency;
 
+/**
+ * 主启动类，运行示例都可以放在这里来做.
+ * <p>
+ *
+ * @author ship
+ * @date 2023/7/4 0004 8:00
+ */
 public class Main {
+    public static void main(String[] args) throws Exception {
+
+    }
 }
--- java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/package-info.java
+++ java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/package-info.java
@@ -1,4 +1,4 @@
 /**
- *
+ * 快速演示 - {@link com.wsp.study.concurrency.quickdemo}
  */
 package com.wsp.study.concurrency;
