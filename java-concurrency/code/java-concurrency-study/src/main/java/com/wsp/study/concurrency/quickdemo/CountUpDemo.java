9f2b3f2bcc62743cdab16c7daff4ef5a6f851a94
ship <594117065@qq.com>
2023-07-06 06:41:44 +0800

    ppt

--- java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/quickdemo/CountUpDemo.java
+++ java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/quickdemo/CountUpDemo.java
@@ -5,6 +5,8 @@ import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 
 import java.io.File;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -20,87 +22,183 @@ public class CountUpDemo {
      * 需要设置日志输出到文件：Edit Run Configurations -> Logs -> save console output to file
      */
     public static final Log log = LogFactory.create().createLog(CountUpDemo.class);
+
     static {
         // 清理日志
         new File("/tmp/logs/idea-console.log").delete();
     }
 
     /**
+     * 屏障，所有 parties 会互相等待，知道全部 parties 都准备好才继续执行
+     */
+    public static CyclicBarrier cyclicBarrier = new CyclicBarrier(12);
+
+    /**
+     * 记录开始时间
+     */
+    public static long startTime;
+
+    /**
      * 合计次数
      */
     public static int count = 0;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        ThreadPoolExecutor threadPoolExecutor = new ExecutorBuilder().setCorePoolSize(10).build();
+        // 同步执行的方案
+        syncProgramme();
+
+        // 异步执行的方案
+        asyncProgramme();
+
+    }
+
+    /**
+     * 示例 - 拧螺丝的并发方案
+     */
+    private static void asyncProgramme() {
+
+        ThreadPoolExecutor threadPoolExecutor = new ExecutorBuilder().setCorePoolSize(12).build();
 
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("小姬拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("大姬拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("小巴拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("大巴拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("小编拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("大编拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("小蔡拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("大蔡拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("小鲲拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("大鲲拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("小拉拧螺丝 + 1");
                 count++;
             }
         });
         threadPoolExecutor.execute(() -> {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                Thread.currentThread().interrupt();
+            }
+            startTime = System.currentTimeMillis();
             for (int i = 0; i < 10000; i++) {
                 log.info("大拉拧螺丝 + 1");
                 count++;
@@ -111,7 +209,9 @@ public class CountUpDemo {
 
         while (true) {
             if (threadPoolExecutor.isTerminated()) {
+                long time = System.currentTimeMillis() - startTime;
                 log.info("=========== 合计：" + count);
+                log.info("== 耗时：" + time);
                 break;
             }
         }
@@ -130,6 +230,63 @@ public class CountUpDemo {
          grep "小拉拧螺丝" idea-console.log | wc -l
          grep "大拉拧螺丝" idea-console.log | wc -l
          */
+    }
 
+    /**
+     * 示例 - 拧螺丝的同步方案
+     */
+    private static void syncProgramme() {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("小姬拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("大姬拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("小巴拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("大巴拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("小编拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("大编拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("小蔡拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("大蔡拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("小鲲拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("大鲲拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("小拉拧螺丝 + 1");
+            count++;
+        }
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("大拉拧螺丝 + 1");
+            count++;
+        }
+        System.out.println("=========== 合计：" + count);
+        System.out.println("== 耗时：" + (System.currentTimeMillis() - startTime));
+        System.exit(0);
     }
 }
