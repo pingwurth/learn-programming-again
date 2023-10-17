9ec5d524392fa4ebcb04605f765b7baa14c30cee
ship <594117065@qq.com>
2023-10-18 00:08:04 +0800

    source code analyze - ThreadLocal#set

--- java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/Main.java
+++ java-concurrency/code/java-concurrency-study/src/main/java/com/wsp/study/concurrency/Main.java
@@ -1,5 +1,7 @@
 package com.wsp.study.concurrency;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 主启动类，运行示例都可以放在这里来做.
  * <p>
@@ -9,6 +11,33 @@ package com.wsp.study.concurrency;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        AtomicInteger i = new AtomicInteger();
+        i.getAndAdd(0x61c88647);
+        i.getAndAdd(0x61c88647);
+        ThreadLocal tl1 = new ThreadLocal();
+        ThreadLocal tl2 = new ThreadLocal();
+        ThreadLocal tl3 = new ThreadLocal();
+        ThreadLocal tl4 = new ThreadLocal();
+        ThreadLocal tl5 = new ThreadLocal();
+        ThreadLocal tl6 = new ThreadLocal();
+        ThreadLocal tl7 = new ThreadLocal();
+        ThreadLocal tl8 = new ThreadLocal();
+        ThreadLocal tl9 = new ThreadLocal();
+        ThreadLocal tl10 = new ThreadLocal();
+        ThreadLocal tl11 = new ThreadLocal();
+        ThreadLocal tl12 = new ThreadLocal();
+        ThreadLocal tl13 = new ThreadLocal();
+        ThreadLocal tl14 = new ThreadLocal();
+        ThreadLocal tl15 = new ThreadLocal();
+        ThreadLocal tl16 = new ThreadLocal();
+        ThreadLocal tl17 = new ThreadLocal();
+        ThreadLocal tl18 = new ThreadLocal();
+        ThreadLocal tl19 = new ThreadLocal();
+        ThreadLocal tl20 = new ThreadLocal();
+        ThreadLocal tl21 = new ThreadLocal();
+        if (false == true) {
+            // \u000a\u007d\u007b
+            System.out.println("true is false");
+        }
     }
 }
